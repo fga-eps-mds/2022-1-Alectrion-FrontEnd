b7788f564baaea614b97a37c8a953fed7991b9e6
wildemberg-sales <junior_sales2010@hotmail.com>
2022-08-06 12:59:19 -0300

    Iniciando criacao da barra de navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { AppRoutes } from './routes/routes'
 import { theme } from './styles/theme'
+import BarraNavegacao from './components/barraNavegacao/index'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <BarraNavegacao />
       <CssBaseline />
       <AppRoutes />
     </ThemeProvider>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,21 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
-import { styled } from '@mui/system'
+// import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+// import { styled } from '@mui/system'
 
-const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.dark
-}))
+// const StyledAppBar = styled(AppBar)(({ theme }) => ({
+//   backgroundColor: theme.palette.primary.dark
+// }))
 
 export default function Home() {
   return (
     <div>
-      <StyledAppBar position="static">
+      {/* <StyledAppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-      </StyledAppBar>
+      </StyledAppBar> */}
       Home
     </div>
   )
