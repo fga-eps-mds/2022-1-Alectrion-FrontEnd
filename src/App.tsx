af83435870b01067821beffb1b0bf342a3a776ce
João Pedro Soares <jpcirqueira81@gmail.com>
2022-08-09 20:36:57 -0300

    Revert "Feature/barra navegacao"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { AppRoutes } from './routes/routes'
 import { theme } from './styles/theme'
-import NavBar from './components/NavBar/index'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <NavBar />
       <CssBaseline />
       <AppRoutes />
     </ThemeProvider>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import { styled } from '@mui/system'
 
-export default function Equipament() {
-  return <div>Bem Vindo a ALECTRION</div>
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.dark
+}))
+
+export default function Home() {
+  return (
+    <div>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </StyledAppBar>
+      Home
+    </div>
+  )
 }
