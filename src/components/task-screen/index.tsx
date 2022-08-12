8dde5944ca4fa3fe92fd116b569581e288c5f91d
PedroSiq <pedroaugustossiqueira@gmail.com>
2022-08-12 20:00:24 -0300

    #90 feat: acrescentando icones e cards

--- src/components/task-screen/index.tsx
+++ src/components/task-screen/index.tsx
@@ -1,20 +1,51 @@
-import { CardContent } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
-import { StyledShortcut } from './styles'
+import {
+  Container,
+  ContainerCard,
+  StyledComputerIcon,
+  StyledPersonIcon,
+  StyledShortcut,
+  StyledBuildIcon
+} from './styles'
+import { Typography } from '@mui/material'
 
-const navigate = useNavigate()
 export const TaskCards = () => {
+  const navigate = useNavigate()
   return (
-    <>
-      <StyledShortcut onClick={() => navigate('/users')}>
-        <CardContent></CardContent>
-      </StyledShortcut>
-      <StyledShortcut onClick={() => navigate('/users')}>
-        <CardContent></CardContent>
-      </StyledShortcut>
-      <StyledShortcut onClick={() => navigate('/users')}>
-        <CardContent></CardContent>
-      </StyledShortcut>
-    </>
+    <Container>
+      <ContainerCard>
+        <StyledShortcut
+          color="#50FF4DB2"
+          onClick={() => navigate('/user-register')}
+          className="Equipment">
+          <StyledComputerIcon style={{ fontSize: 100 }} />
+        </StyledShortcut>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+          Equipamento
+        </Typography>
+      </ContainerCard>
+      <ContainerCard>
+        <StyledShortcut
+          color="#EFCA45"
+          className="Service Order"
+          onClick={() => navigate('/user-register')}>
+          <StyledBuildIcon style={{ fontSize: 100 }} />
+        </StyledShortcut>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+          Ordem de serviço
+        </Typography>
+      </ContainerCard>
+      <ContainerCard>
+        <StyledShortcut
+          color="#71ABDA"
+          className="User"
+          onClick={() => navigate('/user-register')}>
+          <StyledPersonIcon style={{ fontSize: 100 }} />
+        </StyledShortcut>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+          Usuário
+        </Typography>
+      </ContainerCard>
+    </Container>
   )
 }
--- src/pages/task-screen/index.tsx
+++ src/pages/task-screen/index.tsx
@@ -1,28 +1,6 @@
-// import equipmentShortcut from '../../components/task-screen'
-import { Typography, Toolbar, AppBar } from '@mui/material'
-import { Container } from '../user-register/styles'
-import { styled } from '@mui/system'
-import RegisterUserForm from '../../components/register-user-form'
-
-const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  backgroundColor: theme.palette.primary.dark
-}))
+import { TaskCards } from '../../components/task-screen'
 
 const TaskScreen = () => {
-  return (
-    <>
-      <StyledAppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Alectrion
-          </Typography>
-        </Toolbar>
-      </StyledAppBar>
-      <Container>
-        <Typography variant="h3">Cadastro Usuário</Typography>
-        <RegisterUserForm />
-      </Container>
-    </>
-  )
+  return <TaskCards />
 }
 export default TaskScreen
