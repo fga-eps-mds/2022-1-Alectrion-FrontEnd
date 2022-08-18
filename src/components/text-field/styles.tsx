0eaeb84174db107e824b25145216d13fd00dcd5a
mv23c <100789090+mv23c@users.noreply.github.com>
2022-08-18 16:54:33 -0300

    issue 118 feat: ajeitando a height que estava desabilitada.
    Signed-off-by: mv23c <100789090+mv23c@users.noreply.github.com>
    Co-authored-by: .katuner <katunerx@gmail.com>

--- src/components/text-field/styles.tsx
+++ src/components/text-field/styles.tsx
@@ -3,7 +3,7 @@ import { TextField } from '@mui/material'
 
 export const StyledTextField = styledSystem(TextField)(({ theme }) => ({
   // maxWidth: '416px',
-  // height: '40px',
+  height: '40px',
   marginTop: '0px',
   marginBottom: '0px',
 
@@ -17,11 +17,10 @@ export const StyledTextField = styledSystem(TextField)(({ theme }) => ({
   },
   '& .MuiOutlinedInput-notchedOutline': {
     borderRadius: '15px'
-    // borderColor: theme.palette.primary.gray
   },
   '& .MuiInputLabel-shrink': {
     marginTop: '-7px',
-    color: theme.palette.background.default
-    // textShadow: '0 0px 1px rgba(0,0,0,1), 0 0px 1px rgba(0,0,0,1)'
+    color: theme.palette.background.default,
+    textShadow: '0 0px 1px rgba(0,0,0,1), 0 0px 1px rgba(0,0,0,1)'
   }
 }))
