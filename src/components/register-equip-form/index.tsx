4aeac86565314dd1403fe53e32158e96de42c944
PedroSiq <pedroaugustossiqueira@gmail.com>
2022-08-27 04:16:47 -0300

    #76 feat: acrescentados campos de texto na tela

--- src/components/register-equip-form/index.tsx
+++ src/components/register-equip-form/index.tsx
@@ -16,7 +16,7 @@ const RegisterEquipForm = () => {
     serialNumber: yup.string(),
     model: yup.string(),
     acquisitionType: yup.string(),
-    acquisitionDate: yup.string(),
+    acquisitionDate: yup.date(),
     fiscalNote: yup.string(),
     processorType: yup.string(),
     ramMemory: yup.string(),
@@ -85,7 +85,7 @@ const RegisterEquipForm = () => {
             <StyledSelect
               id="productType-select-label"
               data-testid="productType-select"
-              label="Tipo de produto"
+              label="Tipo produto"
               type="text"
               name="productType"
               variant="outlined"
@@ -122,6 +122,104 @@ const RegisterEquipForm = () => {
             error={
               formik.touched.tippingNumber &&
               Boolean(formik.errors.tippingNumber)
+            }
+          />
+          <StyledTextField
+            id="brand-input"
+            label="Marca"
+            type="text"
+            name="brand"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.brand}
+            helperText={formik.touched.brand && formik.errors.brand}
+            error={formik.touched.brand && Boolean(formik.errors.brand)}
+          />
+          <StyledTextField
+            id="serialNumber-input"
+            label="N° Série"
+            type="text"
+            name="serialNumber"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.serialNumber}
+            helperText={
+              formik.touched.serialNumber && formik.errors.serialNumber
+            }
+            error={
+              formik.touched.serialNumber && Boolean(formik.errors.serialNumber)
+            }
+          />
+          <StyledTextField
+            id="model-input"
+            label="N° Tombamento"
+            type="text"
+            name="model"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.model}
+            helperText={formik.touched.model && formik.errors.model}
+            error={formik.touched.model && Boolean(formik.errors.model)}
+          />
+          <StyledTextField
+            id="acquisitionType-input"
+            label="Tipo aquisição"
+            type="text"
+            name="acquisitionType"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.model}
+            helperText={
+              formik.touched.acquisitionType && formik.errors.acquisitionType
+            }
+            error={
+              formik.touched.acquisitionType &&
+              Boolean(formik.errors.acquisitionType)
+            }
+          />
+          <StyledTextField
+            id="equipmentYear-input"
+            label="Ano do equipamento"
+            type="text"
+            name="equipmentYear"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.equipmentYear}
+            helperText={
+              formik.touched.equipmentYear && formik.errors.equipmentYear
+            }
+            error={
+              formik.touched.equipmentYear &&
+              Boolean(formik.errors.equipmentYear)
+            }
+          />
+          <StyledTextField
+            id="acquisitionDate-input"
+            label="Data de aquisição"
+            type="text"
+            name="acquisitionDate"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.acquisitionDate}
+            helperText={
+              formik.touched.acquisitionDate && formik.errors.acquisitionDate
+            }
+            error={
+              formik.touched.acquisitionDate &&
+              Boolean(formik.errors.acquisitionDate)
+            }
+          />
+          <StyledTextField
+            id="fiscalNote-input"
+            label="N° da nota fiscal"
+            type="text"
+            name="model"
+            variant="outlined"
+            onChange={formik.handleChange}
+            value={formik.values.fiscalNote}
+            helperText={formik.touched.fiscalNote && formik.errors.fiscalNote}
+            error={
+              formik.touched.fiscalNote && Boolean(formik.errors.fiscalNote)
             }
           />
           {state === 1 && <h1>teste2</h1>}
