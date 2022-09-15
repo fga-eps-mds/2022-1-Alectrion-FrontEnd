1a18144cba65bdf1b100e0c8625c21c5a05ebe06
mv23c <100789090+mv23c@users.noreply.github.com>
2022-09-14 21:03:50 -0300

    #79 feat: Adição de mais campos, padronização de margin e padding, definição de largura mínima.

--- src/components/equipment-edit-form/index.tsx
+++ src/components/equipment-edit-form/index.tsx
@@ -1,17 +1,157 @@
+import { FormControl, MenuItem } from '@mui/material'
 // import { useState } from 'react'
-import { Container, FormContainer, StyledCard, StyledForm } from './styles'
+// import { useNavigate } from 'react-router-dom'
+import * as yup from 'yup'
+import { toast } from 'react-toastify'
+import api from '../../api/config'
+import { useFormik } from 'formik'
+import {
+  Container,
+  StyledCard,
+  StyledForm,
+  FormContainer,
+  StyledInputLabel,
+  StyledSelect,
+  StyledTextField
+} from './styles'
 
 const EquipmentEditForm = () => {
+  // const navigate = useNavigate()
+  const validationSchema = yup.object().shape({
+    productType: yup.string().required('Esse campo é obrigatório'),
+    tippingNumber: yup.string().required('Esse campo é obrigatório'),
+    brand: yup.string().trim().required('Esse campo é obrigatório'),
+    serialNumber: yup.string().trim().required('Esse campo é obrigatório'),
+    model: yup.string().trim().required('Esse campo é obrigatório'),
+    acquisitionType: yup.string().trim().required('Esse campo é obrigatório')
+  })
+  const formik = useFormik({
+    initialValues: {
+      productType: '',
+      tippingNumber: '',
+      brand: '',
+      serialNumber: '',
+      model: '',
+      acquisitionType: ''
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await api.put('/updateEquipment', {
+          // rota fictícia
+          type: values.productType,
+          tippingNumber: values.tippingNumber
+        })
+        toast.success('Equipamento cadastrado.')
+      } catch (error) {
+        toast.error('Aconteceu algum erro.')
+      }
+    }
+  })
   // const [state, setState] = useState(0)
   return (
     <Container>
       <StyledCard>
-        <StyledForm>
-          <FormContainer></FormContainer>
+        <StyledForm onSubmit={formik.handleSubmit}>
+          <FormContainer>
+            <FormControl fullWidth>
+              <StyledInputLabel id="productType-label">
+                Tipo de Produto
+              </StyledInputLabel>
+              <StyledSelect
+                id="productType-label"
+                data-testid="productType-select"
+                type="text"
+                name="productType"
+                variant="outlined"
+                error={
+                  formik.touched.productType &&
+                  Boolean(formik.errors.productType)
+                }
+                onChange={formik.handleChange}
+                value={formik.values.productType}>
+                <MenuItem>CPU</MenuItem>
+              </StyledSelect>
+            </FormControl>
+
+            <StyledTextField
+              id="tippingNumber-input"
+              label="N° de Tombamento"
+              type="text"
+              name="tippingNumber"
+              variant="outlined"
+              onChange={formik.handleChange}
+              value={formik.values.tippingNumber}
+              helperText={
+                formik.touched.tippingNumber && formik.errors.tippingNumber
+              }
+              error={
+                formik.touched.tippingNumber &&
+                Boolean(formik.errors.tippingNumber)
+              }
+            />
+
+            <StyledTextField
+              id="brand-input"
+              label="Marca"
+              type="text"
+              name="brand"
+              variant="outlined"
+              onChange={formik.handleChange}
+              value={formik.values.brand}
+              helperText={formik.touched.brand && formik.errors.brand}
+              error={formik.touched.brand && Boolean(formik.errors.brand)}
+            />
+
+            <StyledTextField
+              id="serialNumber-input"
+              label="N° de Série"
+              type="text"
+              name="serialNumber"
+              variant="outlined"
+              onChange={formik.handleChange}
+              value={formik.values.serialNumber}
+              helperText={
+                formik.touched.serialNumber && formik.errors.serialNumber
+              }
+              error={
+                formik.touched.serialNumber &&
+                Boolean(formik.errors.serialNumber)
+              }
+            />
+
+            <StyledTextField
+              id="model-input"
+              label="Modelo"
+              type="text"
+              name="model"
+              variant="outlined"
+              onChange={formik.handleChange}
+              value={formik.values.model}
+              helperText={formik.touched.model && formik.errors.model}
+              error={formik.touched.model && Boolean(formik.errors.model)}
+            />
+
+            <StyledTextField
+              id="acquisitionType-input"
+              label="Tipo de Aquisição"
+              type="text"
+              name="acquisitionType"
+              variant="outlined"
+              onChange={formik.handleChange}
+              value={formik.values.model}
+              helperText={
+                formik.touched.acquisitionType && formik.errors.acquisitionType
+              }
+              error={
+                formik.touched.acquisitionType &&
+                Boolean(formik.errors.acquisitionType)
+              }
+            />
+          </FormContainer>
         </StyledForm>
       </StyledCard>
     </Container>
   )
 }
-
 export default EquipmentEditForm
--- src/components/equipment-edit-form/styles.tsx
+++ src/components/equipment-edit-form/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { styled as styledSystem } from '@mui/system'
-import { Card } from '@mui/material'
+import { Card, InputLabel, Select, TextField } from '@mui/material'
 
 export const Container = styled.div`
   display: flex;
@@ -14,21 +14,63 @@ export const StyledCard = styledSystem(Card)(({ theme }) => ({
   backgroundColor: theme.palette.primary.light,
   display: 'flex',
   maxWidth: '1200px',
+  minWidth: '1099.97px',
   margin: 'auto',
-  height: 'max-content'
+  height: 'max-content', // Implica que o conteúdo não será quebrado.
+  borderRadius: '10px'
 }))
 
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 3rem;
-  flex: 1;
+  //gap: 3rem;
+  //flex: 1;
   width: 100%;
-  padding: 2rem;
+  //padding: 2rem;
+  padding: 32px;
 `
 export const FormContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   height: max-content;
-  gap: 3rem;
+  //gap: 2rem;
+  gap: 32px;
 `
+
+export const StyledInputLabel = styledSystem(InputLabel)(({ theme }) => ({
+  marginTop: '-7px'
+}))
+
+export const StyledSelect = styledSystem(Select)(({ theme }) => ({
+  '& .MuiSelect-select': {
+    backgroundColor: theme.palette.background.default,
+    padding: '8.5px',
+    borderRadius: '15px !important',
+    color: '#666666'
+  },
+  '& .MuiOutlinedInput-root': {
+    borderRadius: '15px'
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderRadius: '15px'
+  },
+  backgroundColor: theme.palette.background.default,
+  borderRadius: '15px !important',
+  height: '40px',
+  width: '324px'
+}))
+
+export const StyledTextField = styledSystem(TextField)(({ theme }) => ({
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderRadius: '15px !important'
+  },
+  '& .MuiOutlinedInput-input': {
+    borderRadius: '15px !important',
+    backgroundColor: theme.palette.background.default,
+    height: '7.5px'
+  },
+  maxWidth: '324px',
+  '& .MuiInputLabel-root': {
+    marginTop: '-7px'
+  }
+}))
