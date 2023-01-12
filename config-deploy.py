7c9c6492a40da91d3e15ceca35d6243ae3d2b008
RuanMoura <ruanmoura13@outlook.com>
2023-01-11 21:39:16 -0300

    eslint nao gosta de aspas duplas

--- config-deploy.py
+++ config-deploy.py
@@ -3,6 +3,6 @@ import sys
 file_path = './src/config/env.ts'
 with open(file_path) as fp:
     file = fp.read()
-file = file.replace('process.env.REACT_APP_GATEWAY_URL', f'"{sys.argv[1]}"')
+file = file.replace('process.env.REACT_APP_GATEWAY_URL', f"'{sys.argv[1]}'")
 with open(file_path, 'w') as fp:
     fp.write(file)
