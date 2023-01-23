fe2bf77aa6d0d3b28e80c1f742da8dd7026f6679
Matheus Henrique <97904643+mathonaut@users.noreply.github.com>
2023-01-23 20:54:30 -0300

    #16 feat: Adicionada opção para alterar status da O.S.

---
+++ components/order-services-table/index.tsx
@@ -0,0 +1,98 @@
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+
+import { StyledTableCell, StyledTableRow } from './styles'
+import { dateFormat } from '../../utils/dateFormat'
+import { useNavigate } from 'react-router-dom'
+import { EditOSButton } from '../edit-os-button'
+import OrderServiceUpdateForm  from '../order-service-update-form'
+
+interface OrderService {
+  id: string
+  date: string
+  description: string
+  authorId: string
+  sender: string
+  senderFunctionalNumber: string
+  receiverName: string
+  equipment: {
+    type: string
+    tippingNumber: string
+    status: string
+  }
+}
+interface OrderServicesProps {
+  orderServices: OrderService[]
+  isConsulta: boolean
+}
+export default function OderServiceTable({
+  orderServices,
+  isConsulta
+}: OrderServicesProps) {
+  const navigate = useNavigate()
+  return (
+    <TableContainer
+      sx={{
+        margin: '0 auto',
+        maxWidth: '1286px',
+        textAlign: 'center'
+      }}
+      component={Paper}>
+      <Table aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="center">Id</StyledTableCell>
+            <StyledTableCell align="center">Data de Entrada</StyledTableCell>
+            <StyledTableCell align="center">Tipo Equipamento</StyledTableCell>
+            <StyledTableCell align="center">N° de Tombamento</StyledTableCell>
+            <StyledTableCell align="center">Status do Equipamento</StyledTableCell>
+            <StyledTableCell align="center">Recebedor</StyledTableCell>
+            <StyledTableCell align="center">Entregador</StyledTableCell>
+            <StyledTableCell align="center">Atualizar</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orderServices?.map((orderSerivce) => (
+            <StyledTableRow key={orderSerivce.id}>
+              <StyledTableCell align="center">
+                {orderSerivce.id}
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                {dateFormat(orderSerivce.date)}
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                {orderSerivce.equipment.type}
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                {orderSerivce.equipment.tippingNumber}
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                {orderSerivce.equipment.status}
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                {orderSerivce.receiverName}
+              </StyledTableCell>
+              <StyledTableCell align="center">{`${orderSerivce.sender} - ${orderSerivce.senderFunctionalNumber}`}</StyledTableCell>
+              {!isConsulta && (
+                <StyledTableCell
+                  align="center"
+                  onClick={() =>
+                    navigate('/order-service-update-form', {
+                      state: { orderSerivce }
+                    })
+                  }>
+                  <EditOSButton/>
+                </StyledTableCell>
+              )}
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
---
+++ pages/order-service-edit/index.tsx
@@ -0,0 +1,117 @@
+/* eslint-disable no-unused-vars */
+import { CircularProgress, Typography } from '@mui/material'
+import { AxiosResponse } from 'axios'
+import { useContext, useEffect, useState } from 'react'
+import {
+  Params,
+  useNavigate,
+  useParams,
+  useSearchParams
+} from 'react-router-dom'
+import api from '../../api/config'
+import { AuthContext } from '../../contexts/auth'
+import { Container } from '../user-register/styles'
+import OrderServiceUpdateForm  from '../../components/order-service-update-form'
+
+export type Equipment = {
+  id: string
+  tippingNumber: string
+  serialNumber: string
+  type: string
+  status: string
+  model: string
+  description: string
+  initialUseDate: string
+  screenSize: string
+  invoiceNumber: string
+  power?: string
+  screenType?: string
+  processor: string
+  storageType: string
+  storageAmount: string
+  ram_size: string
+  createdAt: string
+  updatedAt: string
+  formattedStatus: string
+}
+
+const OrderEdit = () => {
+  const navigate = useNavigate()
+  const [equipment, setEquipment] = useState<Equipment | undefined>(undefined)
+  const [units, setUnits] = useState<
+    { id: string; name: string; localization: string }[] | undefined
+  >(undefined)
+
+  const fetchUnits = async () => {
+    const {
+      data
+    }: AxiosResponse<{ id: string; name: string; localization: string }[]> =
+      await api.get(`equipment/getAllUnits`)
+    setUnits(data)
+  }
+
+  useEffect(() => {
+    fetchUnits()
+  }, [])
+
+  const fetchEquipment = async (tippingNumber: string) => {
+    const { data }: AxiosResponse<Equipment> = await api.get(
+      `equipment/listOne/?tippingNumber=${tippingNumber}`
+    )
+
+    let formattedStatus = ''
+    const { status } = data
+    switch (status) {
+      case 'MAINTENANCE':
+        formattedStatus = 'Em Manutenção'
+        break
+      case 'ACTIVE':
+        formattedStatus = 'Ativo'
+        break
+      case 'ACTIVE_BY_DEMISE':
+        formattedStatus = 'Ativo'
+        break
+      case 'INACTIVE':
+        formattedStatus = 'Inativo'
+        break
+      case 'DOWNGRADED':
+        formattedStatus = 'Baixado'
+        break
+      case 'TECHNICAL_RESERVE':
+        formattedStatus = 'Reserva Técnica'
+        break
+      default:
+        break
+    }
+
+    setEquipment({ ...data, formattedStatus })
+  }
+
+  const handleTippingNumberChange = (data: string) => {
+    if (data.length) {
+      fetchEquipment(data)
+    }
+  }
+
+  const { user } = useContext(AuthContext)
+
+  return (
+    <Container>
+      <>
+        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+          Atualização de Ordem de Serviço
+        </Typography>
+        <OrderServiceUpdateForm
+          units={units}
+          initialData={equipment}
+          user={{
+            token: user?.token ?? '',
+            name: user?.name ?? ''
+          }}
+          handleTippingNumberChange={handleTippingNumberChange}
+        />{' '}
+      </>
+    </Container>
+  )
+}
+export default OrderEdit
