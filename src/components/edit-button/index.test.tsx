8b5c098c8f074ae6eb6b29923efb435f74983705
ErickGiffoni <giffoni.erick@gmail.com>
2023-01-24 14:48:42 -0300

    [#102] fix: teste do edit button
    Co-authored-by: JoaoVitorFarias <jvlopesfarias@gmail.com>

--- src/components/edit-button/index.test.tsx
+++ src/components/edit-button/index.test.tsx
@@ -2,13 +2,38 @@ import { EditButton } from './index'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Providers from '../../utils/test-utils'
+import { AuthContext } from '../../contexts/auth'
 
 test('should click edit button', () => {
-  render(
-    <Providers>
-      <EditButton>Test</EditButton>
-    </Providers>
-  )
-  const RegisterButton = screen.getByRole('button', { name: 'Test' })
-  userEvent.click(RegisterButton)
+    const location = {
+        state: {
+            userId: "1",
+        },
+        user: {
+            role: "gerente"
+        }
+    };
+    render(
+        <Providers location={location}>
+            <AuthContext.Provider
+                value={{
+                    isAuthenticated: 'authenticated',
+                    user: {
+                        token: 'wjhqwejk',
+                        expireIn: 'sdfjhsdf',
+                        email: 'user@user.com',
+                        name: 'user',
+                        role: 'administrador'
+                    },
+                    Login: jest.fn(),
+                    Logout: jest.fn()
+                }}>
+                <EditButton /*hidden={true}*/ /*role={'button'}*/>Test</EditButton>
+            </AuthContext.Provider>
+        </Providers>
+    )
+    // problema: useContext do componente pega o user como obj undefined
+    const RegisterButton = screen.getByRole('button', { name: 'Test' })
+    // const RegisterButton = screen.getByTestId('button')
+    userEvent.click(RegisterButton)
 })
