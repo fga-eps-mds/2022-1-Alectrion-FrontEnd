58f0fc6d2f25e3013c1a300b5678be90eced065b
JoaoVitorFarias <jvlopesfarias@gmail.com>
2023-01-29 00:28:02 -0300

    test: atualiza test

--- src/pages/order-services/index.test.tsx
+++ src/pages/order-services/index.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import { OrderServices } from './index'
 import Providers from './../../utils/test-utils'
+import { AuthContext } from '../../contexts/auth'
 
 test('should render equipments data', async () => {
     const location = {
@@ -11,7 +12,21 @@ test('should render equipments data', async () => {
     };
     render(
         <Providers location={location}>
-            <OrderServices />
+            <AuthContext.Provider
+                value={{
+                    isAuthenticated: 'authenticated',
+                    user: {
+                        token: 'wjhqwejk',
+                        expireIn: 'sdfjhsdf',
+                        email: 'user@user.com',
+                        name: 'user',
+                        role: 'administrador'
+                    },
+                    Login: jest.fn(),
+                    Logout: jest.fn()
+                }}>
+                <OrderServices />
+            </AuthContext.Provider>
         </Providers>
     )
 })
